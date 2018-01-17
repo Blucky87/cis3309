e01f5e19589668c6439f9d91fd5f45dfc5cd6f13
Brian L <blucky87@gmail.com>
2018-01-17 01:37:55 -0500

    added esc key functionality

--- 4-1/AreaAndPerimeter/Form1.cs
+++ 4-1/AreaAndPerimeter/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace AreaAndPerimeter
@@ -14,6 +7,9 @@ namespace AreaAndPerimeter
     {
         public Form1()
         {
+            KeyPreview = true;
+            KeyPress += Form1_KeyPress;
+
             InitializeComponent();
         }
 
@@ -28,6 +24,14 @@ namespace AreaAndPerimeter
         private void button2_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar.Equals('\u001b'))
+            {
+                button2_Click(sender, e);
+            }
         }
 
     }
