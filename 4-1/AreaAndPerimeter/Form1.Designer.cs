13b9c43198d024c1863b4413dcb3ee5eb59b1523
Brian L <blucky87@gmail.com>
2018-01-17 01:25:23 -0500

    added 4-1 finished

--- 4-1/AreaAndPerimeter/Form1.Designer.cs
+++ 4-1/AreaAndPerimeter/Form1.Designer.cs
@@ -28,12 +28,138 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.calcButton = new System.Windows.Forms.Button();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.txtLength = new System.Windows.Forms.TextBox();
+            this.txtWidth = new System.Windows.Forms.TextBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtPerimeter = new System.Windows.Forms.TextBox();
+            this.lblLength = new System.Windows.Forms.Label();
+            this.lblWidth = new System.Windows.Forms.Label();
+            this.lblArea = new System.Windows.Forms.Label();
+            this.lblPerimeter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // calcButton
+            // 
+            this.calcButton.Location = new System.Drawing.Point(23, 175);
+            this.calcButton.Name = "calcButton";
+            this.calcButton.Size = new System.Drawing.Size(75, 23);
+            this.calcButton.TabIndex = 0;
+            this.calcButton.Text = "Calculate";
+            this.calcButton.UseVisualStyleBackColor = true;
+            this.calcButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(146, 175);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 1;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtLength
+            // 
+            this.txtLength.Location = new System.Drawing.Point(121, 25);
+            this.txtLength.Name = "txtLength";
+            this.txtLength.Size = new System.Drawing.Size(100, 20);
+            this.txtLength.TabIndex = 2;
+            // 
+            // txtWidth
+            // 
+            this.txtWidth.Location = new System.Drawing.Point(121, 51);
+            this.txtWidth.Name = "txtWidth";
+            this.txtWidth.Size = new System.Drawing.Size(100, 20);
+            this.txtWidth.TabIndex = 3;
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(121, 95);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.ReadOnly = true;
+            this.txtArea.Size = new System.Drawing.Size(100, 20);
+            this.txtArea.TabIndex = 4;
+            // 
+            // txtPerimeter
+            // 
+            this.txtPerimeter.Location = new System.Drawing.Point(121, 121);
+            this.txtPerimeter.Name = "txtPerimeter";
+            this.txtPerimeter.ReadOnly = true;
+            this.txtPerimeter.Size = new System.Drawing.Size(100, 20);
+            this.txtPerimeter.TabIndex = 5;
+            // 
+            // lblLength
+            // 
+            this.lblLength.AutoSize = true;
+            this.lblLength.Location = new System.Drawing.Point(51, 28);
+            this.lblLength.Name = "lblLength";
+            this.lblLength.Size = new System.Drawing.Size(43, 13);
+            this.lblLength.TabIndex = 6;
+            this.lblLength.Text = "Length:";
+            // 
+            // lblWidth
+            // 
+            this.lblWidth.AutoSize = true;
+            this.lblWidth.Location = new System.Drawing.Point(51, 58);
+            this.lblWidth.Name = "lblWidth";
+            this.lblWidth.Size = new System.Drawing.Size(38, 13);
+            this.lblWidth.TabIndex = 7;
+            this.lblWidth.Text = "Width:";
+            // 
+            // lblArea
+            // 
+            this.lblArea.AutoSize = true;
+            this.lblArea.Location = new System.Drawing.Point(51, 98);
+            this.lblArea.Name = "lblArea";
+            this.lblArea.Size = new System.Drawing.Size(32, 13);
+            this.lblArea.TabIndex = 8;
+            this.lblArea.Text = "Area:";
+            // 
+            // lblPerimeter
+            // 
+            this.lblPerimeter.AutoSize = true;
+            this.lblPerimeter.Location = new System.Drawing.Point(51, 124);
+            this.lblPerimeter.Name = "lblPerimeter";
+            this.lblPerimeter.Size = new System.Drawing.Size(54, 13);
+            this.lblPerimeter.TabIndex = 9;
+            this.lblPerimeter.Text = "Perimeter:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(258, 215);
+            this.Controls.Add(this.lblPerimeter);
+            this.Controls.Add(this.lblArea);
+            this.Controls.Add(this.lblWidth);
+            this.Controls.Add(this.lblLength);
+            this.Controls.Add(this.txtPerimeter);
+            this.Controls.Add(this.txtArea);
+            this.Controls.Add(this.txtWidth);
+            this.Controls.Add(this.txtLength);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.calcButton);
+            this.Name = "Area And Perimeter";
+            this.Text = "Area And Perimeter";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button calcButton;
+        private System.Windows.Forms.Button exitButton;
+        private System.Windows.Forms.TextBox txtLength;
+        private System.Windows.Forms.TextBox txtWidth;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.TextBox txtPerimeter;
+        private System.Windows.Forms.Label lblLength;
+        private System.Windows.Forms.Label lblWidth;
+        private System.Windows.Forms.Label lblArea;
+        private System.Windows.Forms.Label lblPerimeter;
     }
 }
 
--- 4-1/AreaAndPerimeter/Form1.cs
+++ 4-1/AreaAndPerimeter/Form1.cs
@@ -16,5 +16,19 @@ namespace AreaAndPerimeter
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtArea.Text = (Decimal.Parse(txtLength.Text) * Decimal.Parse(txtWidth.Text)).ToString();
+            txtPerimeter.Text = ((Decimal.Parse(txtLength.Text) * 2) + (Decimal.Parse(txtWidth.Text) * 2)).ToString();
+
+            txtLength.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
     }
 }
