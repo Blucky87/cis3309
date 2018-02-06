f553e3db45adea604519a5e59903e30298ab4ad0
Brian L <blucky87@gmail.com>
2018-02-06 00:23:03 -0500

    wawa

--- wawa/wawa/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Inherits="wawa.Global" %>
---
+++ wawa/wawa_api/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace wawa_api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] {"value1", "value2"};
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public String Post([FromBody] string value)
+        {
+
+            string thing = value;
+            return "test";
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            Console.WriteLine(id.ToString());
+            Console.WriteLine(value);
+            
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
