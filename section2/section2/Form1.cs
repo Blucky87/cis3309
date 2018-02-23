cdb0c9d5d8c9ef5cc1d18cfc6334e954177c2cde
Brian L <blucky87@gmail.com>
2018-02-23 10:09:09 -0500

    commentscommit

--- section2/section2/Form1.cs
+++ section2/section2/Form1.cs
@@ -1,92 +1,134 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace section2
 {
     public partial class FormStudentScores : Form
     {
+        //holds the students in the program
         private BindingList<Student> studentList;
 
         public FormStudentScores()
         {
             InitializeComponent();
 
-
+            //sets the initial students
             loadStudents(new BindingList<Student>());
         }
 
         private void loadStudents(BindingList<Student> studentList)
         {
+            //set the student list
             this.studentList = studentList;
+            //set the student listbox's datasource
             listBoxStudents.DataSource = this.studentList;
         }
 
-
-
+        //event when item selected in listbox changes
         private void listBoxStudents_SelectedIndexChanged(object sender, EventArgs e)
         {
-            Student student = (Student)listBoxStudents.SelectedItem;
+            //if no selected item return
+            if(listBoxStudents.SelectedIndex == -1 ) return;
+            //create a student from the selected index in the listbox
+            Student student = studentList[listBoxStudents.SelectedIndex];
+            //update the scores
             UpdateScoreStats(student);
         }
 
+        //updates the 3 score stats 
         private void UpdateScoreStats(Student student)
         {
+            //if no student clear data
             if (student == null)
             {
                 clearScoreStats();
                 return;
             }
 
+            //sets the textbox text to approptiate student fields
             textBoxScoreTotal.Text = student.ScoreTotal.ToString();
             textBoxScoreAverage.Text = student.ScoreAverage.ToString();
             textBoxScoreCount.Text = student.ScoreList.Count.ToString();
         }
 
+        //clear score stats
         private void clearScoreStats()
         {
+            //sets score stats textbox texts as empty strings
             textBoxScoreTotal.Text = String.Empty;
             textBoxScoreAverage.Text = String.Empty;
             textBoxScoreCount.Text = String.Empty;
         }
 
+        //event when add is clicked
         private void buttonNew_Click(object sender, EventArgs e)
         {
+            //create add student form
             FormAddStudent addStudent = new FormAddStudent();
+            //display form
             DialogResult result = addStudent.ShowDialog();
 
             switch (result)
             {
                 case DialogResult.OK:
+                    //add new student to studentList
                     studentList.Add((Student)addStudent.Tag);
+                    //select the new student
+                    selectStudent((Student)addStudent.Tag);
                     break;
             }
 
         }
 
+        //event when update clicked
         private void buttonUpdate_Click(object sender, EventArgs e)
         {
             Student student = (Student) listBoxStudents.SelectedItem;
+            //if no student, return
             if (student == null) return;
 
+            //get listbox selected item index
             int index = listBoxStudents.SelectedIndex;
+
+            //create new update student form
             FormUpdateStudent updateStudent = new FormUpdateStudent(student);
+            //display form
             DialogResult result = updateStudent.ShowDialog();
 
             switch (result)
             {
                 case DialogResult.OK:
+                    //remove old student entry
                     studentList.RemoveAt(index);
-                    studentList.Insert(index,(Student)updateStudent.Tag);
+                    //add new updated student entry at old position
+                    studentList.Insert(index,(Student) updateStudent.Tag);
+                    //select the new student
+                    selectStudent((Student)updateStudent.Tag);
                     break;
             }
+        }
 
+        //select a student
+        private void selectStudent(Student student)
+        {
+            //check if student exists
+            if (student != null)
+            {
+                //update the 3 score stats
+                UpdateScoreStats(student);
+                //have the student highlighted in the listbox
+                listBoxStudents.SelectedIndex = studentList.IndexOf(student);
+            }
+        }
+
+        //event when delete is clicked
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            //check if a valid index was selected
+            if(listBoxStudents.SelectedIndex != -1)
+                //remove the student at the selected index
+                studentList.RemoveAt(listBoxStudents.SelectedIndex);
         }
     }
 }
--- section2/section2/Student.cs
+++ section2/section2/Student.cs
@@ -7,24 +7,38 @@ namespace section2
 {
     public class Student
     {
+        //holds all scores for student
         public List<int> ScoreList;
+        //holds student name
         public string Name { get; set; }
-
-        public double ScoreTotal => ScoreList.Aggregate(0, (pInt, nInt) => pInt + nInt);
-        public double ScoreAverage => ScoreTotal/ScoreList.Count;
         
+        //wrapper for total score, starts at 0 then adds current to next int in scorelist
+        public double ScoreTotal => ScoreList.Aggregate(0, (pInt, nInt) => pInt + nInt);
 
+        //wrapper for getting average score
+        public double ScoreAverage
+        {
+            get
+            {
+                double avg = ScoreList.Count > 0 ? ScoreTotal / ScoreList.Count : 0;
+                return avg;
+            }
+            
+        }
+
+        //constructor with name and list of all scores
         public Student(string name, List<int> scoreList)
         {
             Name = name;
             ScoreList = scoreList;
         }
-
+        //constructor with just name
         public Student(string name)
         {
             Name = name;
         }
 
+        //override tostring for easier output in listbox
         public override string ToString()
         {
             StringBuilder stringBuilder = new StringBuilder();
