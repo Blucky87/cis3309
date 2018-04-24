c8ddd7202b19ff143e5fa2db87c8adb15ddde963
Brian L <blucky87@gmail.com>
2018-04-24 12:27:19 -0400

    commit

--- template/IDatabase.cs
+++ template/IDatabase.cs
@@ -1,10 +1,25 @@
-    public interface IDatabase<T>
-    {
-        T Get(int entityId);
-        List<T> GetAll();
-        bool Update(T entity);
-        bool Insert(T entity);
-        bool Delete(int entityId);
+public interface IDatabase<T>
+{
+    T Get(int entityId);
+    List<T> GetAll();
+    bool Update(T entity);
+    bool Insert(T entity);
+    bool Delete(int entityId);
 
-        DbConnection CreateConnection();
-    }
+    DbConnection CreateConnection();
+}
+
+public static string GetConnectionString()
+{
+    // Get the conn string from database.txt file in project root directory - for debugging
+    string s = File.ReadAllText(new DirectoryInfo(Environment.CurrentDirectory).Parent.Parent.FullName + @"\databaseAcc.txt");
+    return s;
+}
+
+
+public static OleDbConnection GetConnection()
+{
+    return new OleDbConnection(GetConnectionString());
+}
+
+//listbox.Items.Add and selected index changed.
